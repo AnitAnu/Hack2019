1429f91db73541e8c93cd9877c1110ba272edd99
AnitAnu <39520051+AnitAnu@users.noreply.github.com>
2019-11-13 14:38:51 +0530

    Update HomeController.cs

--- DellTransactionalHackathon/DellTransactionalHackathon/Controllers/HomeController.cs
+++ DellTransactionalHackathon/DellTransactionalHackathon/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace DellTransactionalHackathon.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home Page Loaded";
 
             return View();
         }
